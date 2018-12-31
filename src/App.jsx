dbaad12c3731ecaf86e7f595429e5a9365a8ed10
Dalin Kim <dalinkim7@gmail.com>
2018-12-31 10:39:19 -0600

    using npm installed client-side libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import IssueList from './IssueList.jsx';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const contentNode = document.getElementById('contents');
 ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
