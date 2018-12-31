a39826192d4859e06cecb7950b38efbfab35de9e
Dalin Kim <dalinkim7@gmail.com>
2018-12-31 11:45:32 -0600

    modified to accept HMR

--- src/App.jsx
+++ src/App.jsx
@@ -4,3 +4,7 @@ import ReactDOM from 'react-dom';
 
 const contentNode = document.getElementById('contents');
 ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
+
+if (module.hot) {
+    module.hot.accept();
+}
