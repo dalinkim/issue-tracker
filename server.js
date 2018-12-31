9bde26e342110e7d570aa6352d0a17e4e1f2d02e
Dalin Kim <dalinkim7@gmail.com>
2018-12-30 20:50:13 -0600

    issue validation

--- server.js
+++ server.js
@@ -18,19 +18,61 @@ const issues = [
     },
 ];
 
+app.get('/api/issues', (req, res) => {
+    const metadata = { total_count: issues.length };
+    res.json({ _metadata: metadata, records: issues });
+});
+
+// validIssueStatus and issueFieldType are global objects
+// these are a kind of schema definition to indicate what is a valid issue object
+const validIssueStatus = {
+    New: true,
+    Open: true,
+    Assigned: true,
+    Fixed: true,
+    Verified: true,
+    Closed: true,
+};
+const issueFieldType = {
+    id: 'required',
+    status: 'required',
+    owner: 'required',
+    effort: 'optional',
+    created: 'required',
+    completionDate: 'optional',
+    title: 'required',
+};
+// validateIssue checks against the specification and returns an error if the validation fails.
+function validateIssue(issue) {
+    for (const field in issueFieldType) {
+        const type = issueFieldType[field];
+        if (!type) {
+            // any fields that do not belong are deleted.
+            delete issue[field];
+        } else if (type === 'required' && !issue[field]) {
+            return `${field} is required.`;
+        }
+    }
+    if (!validIssueStatus[issue.status])
+        return `${issue.status} is not a valid status`;
+
+    return null;
+}
+
 app.post('/api/issues', (req, res) => {
     const newIssue = req.body;
     newIssue.id = issues.length + 1;
     newIssue.created = new Date();
     if (!newIssue.status)
         newIssue.status = 'New';
+
+    const err = validateIssue(newIssue);
+    if (err) {
+        res.status(422).json({ message: `Invalid request: ${err}` });
+        return;
+    }
     issues.push(newIssue);
     res.json(newIssue);
-});
-
-app.get('/api/issues', (req, res) => {
-    const metadata = { total_count: issues.length };
-    res.json({ _metadata: metadata, records: issues });
 });
 
 app.listen(3000, function () {
