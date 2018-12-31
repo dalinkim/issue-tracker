d518f053c630eadfd2004eae7d0b7e8a6a25fd8b
Dalin Kim <dalinkim7@gmail.com>
2018-12-31 11:03:15 -0600

    separate bundle configurations

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
+// webpack module needed for the plugin
+const webpack = require('webpack');
+
 module.exports = {
-    entry: './src/App.jsx',
+    entry: {
+        app: './src/App.jsx',
+        vendor: ['react', 'react-dom', 'whatwg-fetch', 'babel-polyfill'],
+    },
     output: {
         path: './static',
         filename: 'app.bundle.js'
     },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+    ],
     module: {
-        loaders: [
-            {
-                test: /\.jsx$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                }
-            },
-        ]
+        loaders: [{
+            test: /\.jsx$/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015']
+            }
+        }, ]
     }
 };
