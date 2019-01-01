c66593c538b48f1c03729797ab6d44d3f00747ca
Dalin Kim <dalinkim7@gmail.com>
2018-12-31 22:48:12 -0600

    common header and footer using nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,36 @@ import IssueEdit from './IssueEdit.jsx';
 const contentNode = document.getElementById('contents');
 const NoMatch = () => <p>Page Not Found</p>;
 
+const App = (props) => (
+  <div>
+    <div className="header">
+      <h1>Issue Tracker</h1>
+    </div>
+    <div className="contents">
+      {props.children}
+    </div>
+    <div className="footer">
+      Full source code available at this <a href="https://github.com/dalinkim/issue-tracker">
+      GitHub repository</a>.
+    </div>
+  </div>
+);
+
+App.propTypes = {
+  children: React.PropTypes.object.isRequired,
+};
+
 // kind of history to use, hashHistory from react-router
-// wrapping IssueList with withRouter - IssueList can use this.props.router to access the router object.
+// wrapping IssueList with withRouter
+// IssueList can use this.props.router to access the router object.
 const RoutedApp = () => (
   <Router history={hashHistory} >
     <Redirect from="/" to="issues" />
-    <Route path="/issues" component={withRouter(IssueList)} />
-    <Route path="/issues/:id" component={IssueEdit} />
-    <Route path="*" component={NoMatch} />
+    <Route path="/" component={App} >
+      <Route path="/issues" component={withRouter(IssueList)} />
+      <Route path="/issues/:id" component={IssueEdit} />
+      <Route path="*" component={NoMatch} />
+    </Route>
   </Router>
 );
 
