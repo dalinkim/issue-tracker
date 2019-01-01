d00fb248cdc3a3e7d07ee1823ac9d23087c284ef
Dalin Kim <dalinkim7@gmail.com>
2019-01-01 01:35:55 -0600

    new Express route for Get API

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ import 'babel-polyfill';
 import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import Issue from './issue.js';
 
 const app = express();
@@ -61,7 +61,32 @@ app.post('/api/issues', (req, res) => {
   });
 });
 
-// new express route has to be placed after all the other routes
+// new Express route for Get API
+app.get('/api/issues/:id', (req, res) => {
+  let issueId;
+  try {
+    issueId = new ObjectId(req.params.id);
+  } catch (error) {
+    res.status(422).json({ message: `Invalid issue ID format: ${error}` });
+    return;
+  }
+
+  db.collection('issues').find({ _id: issueId }).limit(1)
+  .next()
+  .then(issue => {
+    if (!issue) {
+      res.status(404).json({ message: `No such issue: ${issueId}` });
+    } else {
+      res.json(issue);
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(500).json({ message: `Internal Server Error: ${error}` });
+  });
+});
+
+// new Express route has to be placed after all the other routes
 // so that it gets resolved only if none of the previous routes match
 // resolve is also used because sendFile accepts only absolute paths.
 app.get('*', (req, res) => {
